c605ceae3f47e42938daa02edbf980528448d9f4
Cédric RICARD <ricard@free.fr>
2015-03-04 08:46:54 +0100

    Auto-reload logging configuration on changes

--- common/cm_logging.py
+++ common/cm_logging.py
@@ -17,15 +17,17 @@
 
 import os
 import sys
+import logging, logging.config, logging.handlers
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler, FileSystemEventHandler
 
 __author__ = 'ricard'
-
+_watchdog_configured = False
 
 def configure_logging(config, log_name, LOG_CONFIG_PATH, LOG_PATH, DEFAULT_LOG_FORMAT, RUNNING_UNITTEST):
     if not os.path.exists(LOG_PATH):
         os.makedirs(LOG_PATH)
 
-    import logging, logging.config, logging.handlers
 
     if RUNNING_UNITTEST and not logging.Logger.manager.loggerDict:
         # HACK to avoid logs due to DEBUG level forced by pyreadline's logger module
@@ -43,14 +45,44 @@ def configure_logging(config, log_name, LOG_CONFIG_PATH, LOG_PATH, DEFAULT_LOG_F
         return
 
     if not RUNNING_UNITTEST:
+        handler = LoggingConfigEventHandler(log_name, LOG_CONFIG_PATH, LOG_PATH, DEFAULT_LOG_FORMAT)
+        handler.do_reconfigure_logging()
+
+        observer = Observer()
+        # observer.schedule(LoggingEventHandler(), LOG_CONFIG_PATH, recursive=False)
+        observer.schedule(handler, LOG_CONFIG_PATH, recursive=False)
+        print "log watcher to '%s'" % LOG_CONFIG_PATH
+        observer.daemon = True  # should not block the program ending
+        observer.start()
+
+
+class LoggingConfigEventHandler(FileSystemEventHandler):
+    """Check for logging config changes."""
+    def __init__(self, log_name, LOG_CONFIG_PATH, LOG_PATH, DEFAULT_LOG_FORMAT):
+        self.log_name = log_name
+        self.config_path = LOG_CONFIG_PATH
+        self.log_path = LOG_PATH
+        self.default_log_format = DEFAULT_LOG_FORMAT
+        self.base_filename = 'logging-%s' % self.log_name
+
+    def on_any_event(self, event):
+        print("Event %s" % repr(event))
+        logging.debug("Event %s", event)
+        if not event.is_directory:
+            filename = os.path.basename(event.src_path)
+            if filename.startswith(self.base_filename):
+                logging.info("Logging config change detected. Reloading configuration...")
+                self.do_reconfigure_logging()
+
+    def do_reconfigure_logging(self):
         log_ok = False
-        logconfig_fullpath = os.path.join(LOG_CONFIG_PATH, 'logging-%s.py' % log_name)
+        logconfig_fullpath = os.path.join(self.config_path, 'logging-%s.py' % self.log_name)
         if not os.path.exists(logconfig_fullpath):
             logconfig_fullpath = os.path.splitext(logconfig_fullpath)[0] + '.ini'
         if not os.path.exists(logconfig_fullpath):
             logconfig_fullpath = os.path.splitext(logconfig_fullpath)[0] + '.default.py'
         if os.path.exists(logconfig_fullpath):
-            # print "Loading log config from %s" % logconfig_fullpath
+            logging.info("Loading log config from %s", logconfig_fullpath)
             try:
                 if logconfig_fullpath.endswith(".py"):
                     import imp
@@ -69,9 +101,9 @@ def configure_logging(config, log_name, LOG_CONFIG_PATH, LOG_PATH, DEFAULT_LOG_F
             print "Loading log config not found or badly formated, loading default config."
 
             log_level = logging.INFO
-            formatter = logging.Formatter(DEFAULT_LOG_FORMAT)
-            logging.basicConfig(level=log_level, format=DEFAULT_LOG_FORMAT)
-            logFile = logging.handlers.RotatingFileHandler(os.path.join(LOG_PATH, os.path.basename(log_name) + '.log'),
+            formatter = logging.Formatter(self.default_log_format)
+            logging.basicConfig(level=log_level, format=self.default_log_format)
+            logFile = logging.handlers.RotatingFileHandler(os.path.join(self.log_path, os.path.basename(self.log_name) + '.log'),
                                                            maxBytes=1 * 1024 * 1024, backupCount=10)
             logFile.setLevel(log_level)
             logFile.setFormatter(formatter)
@@ -80,7 +112,7 @@ def configure_logging(config, log_name, LOG_CONFIG_PATH, LOG_PATH, DEFAULT_LOG_F
 
             logger = logging.getLogger('mailing.out')
             logger.propagate = False
-            logFile = logging.handlers.TimedRotatingFileHandler(os.path.join(LOG_PATH, 'mailing.out.log'), when='d',
+            logFile = logging.handlers.TimedRotatingFileHandler(os.path.join(self.log_path, 'mailing.out.log'), when='d',
                                                                 interval=1, backupCount=20)
             logFile.setLevel(log_level)
             logFile.setFormatter(formatter)
