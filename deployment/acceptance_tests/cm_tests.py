a01eb37e9ab63db7f118fc159e05e3f444e54806
Cédric RICARD <ricard@free.fr>
2016-08-31 03:25:37 +0200

    Improve big mailing test to create several mailings

--- deployment/acceptance_tests/cm_tests.py
+++ deployment/acceptance_tests/cm_tests.py
@@ -332,7 +332,37 @@ class CloudMailingsTestCase(unittest.TestCase):
             yield recipient
 
     # @unittest.skip("Too long. Should be run manually.")
-    def test_run_big_mailing(self):
+    def test_run_big_mailings(self):
+        nb_mailings = test_config.getint("SETTINGS", "total_mailings", 10)
+        all_mailings = []
+
+        for i in range(nb_mailings):
+            mailing_id, total_recipients = self._create_mailing()
+            all_mailings.append(mailing_id)
+
+        # self.cloudMailingsRpc.mailing_manager_force_check()
+        #time.sleep(6)
+        mailings = self.cloudMailingsRpc.list_mailings({'id': all_mailings})
+        t0 = time.time()
+        while sum(map(lambda mailing: mailing['total_pending'], mailings)) > 0:
+            for mailing in mailings:
+                self.assertTrue(mailing['status'] in ('READY', 'RUNNING', 'FINISHED'))
+            self.assertLess(time.time() - t0, 86400) # 1 day max
+            # self.cloudMailingsRpc.mailing_manager_force_check()
+            time.sleep(10)
+            # self.cloudMailingsRpc.update_statistics()
+            mailings = self.cloudMailingsRpc.list_mailings({'id': all_mailings})
+        # stats not up to date
+        # self.cloudMailingsRpc.update_statistics()
+        mailings = self.cloudMailingsRpc.list_mailings({'id': all_mailings})
+        #self.assertEqual(mailing['status'], 'FINISHED')
+        #print repr(mailing)
+        self.assertEqual(sum(map(lambda mailing: mailing['total_recipient'], mailings)), total_recipients)
+        self.assertEqual(sum(map(lambda mailing: mailing['total_pending'], mailings)), 0)
+        self.assertEqual(sum(map(lambda mailing: mailing['total_sent'], mailings)), total_recipients)
+        self.assertEqual(sum(map(lambda mailing: mailing['total_error'], mailings)), 0)
+
+    def _create_mailing(self):
         mailing_count = len(self.cloudMailingsRpc.list_mailings(self.domain_name))
         email_filename = os.path.join('data', 'email.rfc822')
         self._check_domain_sender(email_filename)
@@ -342,11 +372,9 @@ class CloudMailingsTestCase(unittest.TestCase):
         mailings = self.cloudMailingsRpc.list_mailings(self.domain_name)
         self.assertEquals(len(mailings), mailing_count + 1)
         self.assertEquals(mailings[-1]['mail_from'], mail_from)
-
         self.cloudMailingsRpc.set_mailing_properties(mailing_id, {'scheduled_duration': 1440, 'testing': True,
                                                                   'dont_close_if_empty': True})
         self.cloudMailingsRpc.start_mailing(mailing_id)
-
         total_recipients = TOTAL_RECIPIENTS_FOR_BIG_MAILING
         recipients_list = self._generate_recipients_list(total_recipients)
         rcpts_list2 = []
@@ -367,32 +395,10 @@ class CloudMailingsTestCase(unittest.TestCase):
                 rcpts_list2 = []
         if len(rcpts_list2):
             _add_recipients(rcpts_list2)
-
         mailing = self.cloudMailingsRpc.list_mailings(self.domain_name)[-1]
         self.assertEqual(mailing['total_recipient'], total_recipients)
-
         self.cloudMailingsRpc.set_mailing_properties(mailing_id, {'dont_close_if_empty': False})
-
-        # self.cloudMailingsRpc.mailing_manager_force_check()
-        #time.sleep(6)
-        mailing = self.cloudMailingsRpc.list_mailings(self.domain_name)[-1]
-        t0 = time.time()
-        while mailing['total_pending'] > 0:
-            self.assertTrue(mailing['status'] in ('READY', 'RUNNING'))
-            self.assertLess(time.time() - t0, 86400) # 1 day max
-            # self.cloudMailingsRpc.mailing_manager_force_check()
-            time.sleep(10)
-            # self.cloudMailingsRpc.update_statistics()
-            mailing = self.cloudMailingsRpc.list_mailings(self.domain_name)[-1]
-        # stats not up to date
-        # self.cloudMailingsRpc.update_statistics()
-        mailing = self.cloudMailingsRpc.list_mailings(self.domain_name)[-1]
-        #self.assertEqual(mailing['status'], 'FINISHED')
-        #print repr(mailing)
-        self.assertEqual(mailing['total_recipient'], total_recipients)
-        self.assertEqual(mailing['total_pending'], 0)
-        self.assertEqual(mailing['total_sent'], total_recipients)
-        self.assertEqual(mailing['total_error'], 0)
+        return mailing_id, total_recipients
 
     def test_get_full_reports_with_email_content(self):
         mailing_count = len(self.cloudMailingsRpc.list_mailings(self.domain_name))
